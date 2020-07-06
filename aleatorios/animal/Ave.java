ceb350eac2f53bdb48db78ddaba3be5d61d57c80
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-06 18:15:56 -0300

    Classes e Métodos ABSTRATOS

---
+++ aleatorios/animal/Ave.java
@@ -0,0 +1,7 @@
+package aleatorios.animal;
+
+public abstract class Ave extends Animal {
+
+	public abstract String voar();
+	
+}
---
+++ aleatorios/animal/Mamifero.java
@@ -0,0 +1,7 @@
+package aleatorios.animal;
+
+public abstract class Mamifero extends Animal {
+
+	public abstract String amamentar();
+	
+}
