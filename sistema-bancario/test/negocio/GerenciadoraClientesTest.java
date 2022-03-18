5656d1e2575f3f448c26355056cd5890be4ad0ed
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-03-18 11:20:25 -0300

    test: adicionado teste remove cliente por id informado

--- sistema-bancario/test/negocio/GerenciadoraClientesTest.java
+++ sistema-bancario/test/negocio/GerenciadoraClientesTest.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class GerenciadoraClientesTest {
+
+    private GerenciadoraClientes gerenciadoraClientes;
 
     @Test
     public void testPesquisaCliente() {
@@ -20,7 +22,7 @@ public class GerenciadoraClientesTest {
         clientesDoBanco.add(cliente01);
         clientesDoBanco.add(cliente02);
 
-        GerenciadoraClientes gerenciadoraClientes = new GerenciadoraClientes(clientesDoBanco);
+        gerenciadoraClientes = new GerenciadoraClientes(clientesDoBanco);
 
         // Act
         Cliente cliente = gerenciadoraClientes.pesquisaCliente(1);
@@ -28,5 +30,26 @@ public class GerenciadoraClientesTest {
         // Assert
         assertThat(cliente.getId(), is(1));
         assertThat(cliente.getEmail(), is("gugafarias@gmail.com"));
+    }
+
+    @Test
+    public void testRemoveCliente() {
+        // Arrange
+        Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
+        Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+
+        List<Cliente> clientesDoBanco = new ArrayList<>();
+        clientesDoBanco.add(cliente01);
+        clientesDoBanco.add(cliente02);
+
+        gerenciadoraClientes = new GerenciadoraClientes(clientesDoBanco);
+
+        // Act
+        boolean clienteRemovido = gerenciadoraClientes.removeCliente(2);
+
+        // Assert
+        assertThat(clienteRemovido, is(true));
+        assertThat(gerenciadoraClientes.getClientesDoBanco().size(), is(1));
+        assertNull(gerenciadoraClientes.pesquisaCliente(2));
     }
 }
