7179309c40100c4d3841adeb972bcd2abf598a3e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-03-21 12:19:48 -0300

    test: refatoração dos testes

--- sistema-bancario/test/negocio/GerenciadoraClientesTest.java
+++ sistema-bancario/test/negocio/GerenciadoraClientesTest.java
@@ -1,5 +1,6 @@
 package negocio;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,12 +14,14 @@ import static org.junit.Assert.assertThat;
 public class GerenciadoraClientesTest {
 
     private GerenciadoraClientes gerenciadoraClientes;
+    private final int idCliente01 = 1;
+    private final int idCliente02 = 2;
 
     @Before
     public void setUp() {
         // Arrange
-        Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
-        Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+        Cliente cliente01 = new Cliente(idCliente01, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
+        Cliente cliente02 = new Cliente(idCliente02, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
 
         List<Cliente> clientesDoBanco = new ArrayList<>();
         clientesDoBanco.add(cliente01);
@@ -27,24 +30,29 @@ public class GerenciadoraClientesTest {
         gerenciadoraClientes = new GerenciadoraClientes(clientesDoBanco);
     }
 
+    @After
+    public void tearDown() {
+        gerenciadoraClientes.limpa();
+    }
+
     @Test
     public void testPesquisaCliente() {
         // Act
         Cliente cliente = gerenciadoraClientes.pesquisaCliente(1);
 
         // Assert
-        assertThat(cliente.getId(), is(1));
+        assertThat(cliente.getId(), is(idCliente01));
         assertThat(cliente.getEmail(), is("gugafarias@gmail.com"));
     }
 
     @Test
     public void testRemoveCliente() {
         // Act
-        boolean clienteRemovido = gerenciadoraClientes.removeCliente(2);
+        boolean clienteRemovido = gerenciadoraClientes.removeCliente(idCliente02);
 
         // Assert
         assertThat(clienteRemovido, is(true));
         assertThat(gerenciadoraClientes.getClientesDoBanco().size(), is(1));
-        assertNull(gerenciadoraClientes.pesquisaCliente(2));
+        assertNull(gerenciadoraClientes.pesquisaCliente(idCliente02));
     }
 }
