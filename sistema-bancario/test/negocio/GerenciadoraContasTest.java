69ee43226f763271a89e7d1aa7d1838047517421
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-03-21 14:22:17 -0300

    test: refatoração e adição de novos testes

--- sistema-bancario/test/negocio/GerenciadoraContasTest.java
+++ sistema-bancario/test/negocio/GerenciadoraContasTest.java
@@ -1,6 +1,5 @@
 package negocio;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,41 +11,120 @@ import static org.junit.Assert.assertTrue;
 
 public class GerenciadoraContasTest {
 
-    private GerenciadoraContas gerenciadoraContas;
-    private ContaCorrente conta01;
-    private ContaCorrente conta02;
+    private GerenciadoraContas gerContas;
 
-    @Before
-    public void setUp() {
+    @Test
+    public void testTransfereValor() {
         // Arrange
-        conta01 = new ContaCorrente(1, 200, true);
-        conta02 = new ContaCorrente(2, 0, true);
+        int idConta01 = 1;
+        int idConta02 = 2;
+        ContaCorrente conta01 = new ContaCorrente(idConta01, 200, true);
+        ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
 
         List<ContaCorrente> contasDoBanco = new ArrayList<>();
         contasDoBanco.add(conta01);
         contasDoBanco.add(conta02);
 
-        gerenciadoraContas = new GerenciadoraContas(contasDoBanco);
-    }
+        gerContas = new GerenciadoraContas(contasDoBanco);
 
-    @Test
-    public void testTransfereValor() {
         // Act
-        gerenciadoraContas.transfereValor(1, 100, 2);
+        boolean sucesso = gerContas.transfereValor(idConta01, 100, idConta02);
 
         // Assert
+        assertTrue(sucesso);
         assertThat(conta02.getSaldo(), is(100.0));
         assertThat(conta01.getSaldo(), is(100.0));
     }
 
     @Test
     public void testTransfereValorComSaldoInsuficiente() {
+        // Arrange
+        int idConta01 = 1;
+        int idConta02 = 2;
+        ContaCorrente conta01 = new ContaCorrente(idConta01, 100, true);
+        ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+
+        List<ContaCorrente> contasDoBanco = new ArrayList<>();
+        contasDoBanco.add(conta01);
+        contasDoBanco.add(conta02);
+
+        gerContas = new GerenciadoraContas(contasDoBanco);
+
         // Act
-        boolean sucesso = gerenciadoraContas.transfereValor(1, 300, 2);
+        boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
 
         // Assert
         assertTrue(sucesso);
         assertThat(conta01.getSaldo(), is(-100.0));
-        assertThat(conta02.getSaldo(), is(300.0));
+        assertThat(conta02.getSaldo(), is(200.0));
+    }
+
+    @Test
+    public void testTransfereValorComSaldoNegativo() {
+        // Arrange
+        int idConta01 = 1;
+        int idConta02 = 2;
+        ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+        ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+
+        List<ContaCorrente> contasDoBanco = new ArrayList<>();
+        contasDoBanco.add(conta01);
+        contasDoBanco.add(conta02);
+
+        gerContas = new GerenciadoraContas(contasDoBanco);
+
+        // Act
+        boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+
+        // Assert
+        assertTrue(sucesso);
+        assertThat(conta01.getSaldo(), is(-300.0));
+        assertThat(conta02.getSaldo(), is(200.0));
+    }
+
+    @Test
+    public void testTransfereValorComSaldoNegativoParaNegativo() {
+        // Arrange
+        int idConta01 = 1;
+        int idConta02 = 2;
+        ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+        ContaCorrente conta02 = new ContaCorrente(idConta02, -100, true);
+
+        List<ContaCorrente> contasDoBanco = new ArrayList<>();
+        contasDoBanco.add(conta01);
+        contasDoBanco.add(conta02);
+
+        gerContas = new GerenciadoraContas(contasDoBanco);
+
+        // Act
+        boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+
+        // Assert
+        assertTrue(sucesso);
+        assertThat(conta01.getSaldo(), is(-300.0));
+        assertThat(conta02.getSaldo(), is(100.0));
+    }
+
+    @Test
+    public void testTransfereValorNenhum() {
+        // Arrange
+        int idConta01 = 1;
+        int idConta02 = 2;
+        ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+        ContaCorrente conta02 = new ContaCorrente(idConta02, -100, true);
+
+        List<ContaCorrente> contasDoBanco = new ArrayList<>();
+        contasDoBanco.add(conta01);
+        contasDoBanco.add(conta02);
+
+        gerContas = new GerenciadoraContas(contasDoBanco);
+
+        // Act
+        boolean sucesso = gerContas.transfereValor(idConta01, 2, idConta02);
+
+        // Assert
+        assertTrue(sucesso);
+        assertThat(conta01.getSaldo(), is(-102.0));
+        assertThat(conta02.getSaldo(), is(-98.0));
     }
 }
